efb0892124e648e8fb18f5c891156770f87cec6b
Joanne Basa <joannegbasa@gmail.com>
2017-11-06 11:28:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var increaseRankBy = (n) => {
   const rankedLists = document.querySelectorAll('.ranked-list');
 
   for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
+    let children = rankedLists[i].children;
 
     for (let j = 0, k = children.length; j < k; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n;
@@ -26,6 +26,6 @@ var deepestChild = () => {
     node = nextNode;
     nextNode = node.children[0];
   }
-  
+
   return node;
 }
